aef6ca37bf7c282752a38d1eac14ad8f185b3908
Samuli Aalto <samuli.aalto@hyvanmielenpelit.fi>
2023-09-09 19:16:29 +0300

    mainv1katto2
    mainv1katto2

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1848397.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 148560481, 148560537, 148560549, 148560550, 148560551, 148560552, 148560557, 148560562, 148560564, 148560581 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/148560483/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/148560489/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/148560485/1/ammo.js', 'preload' : true},
+];
